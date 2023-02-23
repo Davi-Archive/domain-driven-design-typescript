32590ab54638cfc10fd0524dbd71e63e246b4ded
d1av <davi4alves@gmail.com>
2023-02-23 02:51:47 -0300

    feat: :sparkles: add new validators

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -2,30 +2,39 @@ class Customer {
 
     _id: string;
     _name: string;
-    _address: string;
+    _address = "";
+    _active = false;
 
-    constructor(id: string, name: string, address: string) {
+    constructor(id: string, name: string, active: boolean) {
         this._id = id;
         this._name = name;
-        this._address = address;
+        this._active = active;
+
+        this.validate();
     }
 
-    get id(): string {
-        return this._id;
+    validate(): void {
+        if (this._name.length === 0) {
+            throw new Error("Name cannot be empty");
+        }
+        if (this._address.length === 0) {
+            throw new Error("Address cannot be empty");
+        }
     }
 
-    get name(): string {
-        return this._name;
-    }
-    set name(name: string) {
+    changeName(name: string) {
         this._name = name;
+        this.validate();
     }
 
-    get address(): string {
-        return this._address;
+    activate() {
+        if (this._address.length === 0) {
+            throw new Error("Address is mandatory to activate customer");
+        }
+        this._active = true;
     }
 
-    set address(address: string) {
-        this._address = address;
+    deactivate() {
+        this._active = false;
     }
 }
