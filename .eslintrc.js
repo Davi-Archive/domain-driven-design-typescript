af15ce93857c7d3952e5e8d478aa3eff61b368d3
d1av <davi4alves@gmail.com>
2023-02-23 02:20:31 -0300

    config: :construction: adding dependencies for typescript-eslint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+  ],
+  plugins: ["@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: "./",
+  },
+  root: true,
+};
